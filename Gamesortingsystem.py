31e7cf9fdc69073deeaef56efb8b63a8805d3abd
Sri (Desktop Admin) <dST02170@galvinparksc.vic.edu.au>
2023-07-17 11:30:02 +1000

    Windowedits

--- Gamesortingsystem.py
+++ Gamesortingsystem.py
@@ -1,31 +1,26 @@
 from tkinter import *
 
-window = Tk()
-window.configure(width=750, height=500)
-window.configure(bg='#494444')
+def validateLogin():
+    username = "use"
+    password = "pas"
+    return
 
-def login():
-    username = "makeuseof"
-    password = "muo"
+#Opens window
+tkwindow = Tk()
+tkwindow.configure(width=750, height=500)
+tkwindow.configure(bg='#494444')
+#Shows title
+title_label = Label(text = 'Video Game Sorting System', font = ('Gotham', 16), fg = 'black', bg = 'light gray').place(x =250, y = 25)
+#Username label & entry box
+username_label = Label(tkwindow, text = 'Username', font = ('Arial', 16), fg = 'black', bg = 'light gray').place(x = 325, y = 100)
+username = StringVar()
+usernameEntry = Entry(font=('Verdana', 16), width=10, textvariable = username).place(x =315, y = 150)
+#Password label & entry box
+password_label = Label(tkwindow, text = 'Password', font = ('Arial', 16), fg = 'black', bg = 'light gray').place(x = 325, y = 200)
+password = StringVar()
+passwordEntry = Entry(font=('Verdana', 16), width=10, textvariable = password, show = '*').place(x = 315, y = 250)
 
-    if username_label.get()==username and password_entry.get()==password:
-        messagebox.showinfo(title="Login Successful!", message="You successfully logged in.")
-    else:
-        messagebox.showerror(title="Error", message="Invalid login.")
-
-title_label = Label(text = 'Video Game Sorting System', font = ('Gotham', 16), fg = 'black', bg = 'light gray')
-username_label = Label(text = 'Username', font = ('Arial', 16), fg = 'black', bg = 'light gray')
-password_label = Label(text = 'Password', font = ('Arial', 16), fg = 'black', bg = 'light gray')
-
-login_button = Button(text='Enter', font=('Arial', 10),command=login)
-
-entry = Entry(font=('Verdana', 16), width=10)
-entry2 = Entry(font=('Verdana', 16), width=10)
-
-title_label.place(x =250, y = 25)
-username_label.place(x = 325, y = 100)
-password_label.place(x = 325, y = 200)
-entry.place(x =315, y = 150)
-entry2.place(x =315, y = 250)
-login_button.place(x = 315, y = 300)
+#Log-in button
+login_button = Button(text='Enter', font=('Arial', 10)).place(x = 350, y = 300)
+#Loops the code to make a permenant window
 mainloop()
